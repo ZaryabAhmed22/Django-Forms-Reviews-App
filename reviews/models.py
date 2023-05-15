edc3c071a7f972dcbd0d14cd7f89751cc357a9a0
ZaryabAhmed22 <79729046+ZaryabAhmed22@users.noreply.github.com>
2023-05-15 23:16:05 +0500

    Adding form data to model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 # Create your models here.
+
+
+class Review(models.Model):
+    user_name = models.CharField(max_length=50)
+    review_text = models.TextField(max_length=200)
+    rating = models.IntegerField(
+        validators=[MinLengthValidator(1), MaxLengthValidator(5)])
+
+    def __str__(self):
+        return f"{self.user_name}, {self.review_text}, {self.rating}"
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
+from .models import Review
 # Create your views here.
 
 
@@ -11,11 +12,14 @@ def review(request):
     #     print(entered_username)
     #     return HttpResponseRedirect("/thank-you")
 
-    # >> Collecting data using Django | in this code we are checking if the request method is post, and if it is post, we are creating a new form using the data entered by the user "request.POST" (is the data), then we will check if the form submission was valid using the bilt in function is_valid and send a redirect and print the data "form.cleaned_data".
+    # >> Collecting data using Django | in this code we are checking if the request method is post, and if it is post, we are creating a new form using the data entered by the user "request.POST" (is the data), then we will check if the form submission was valid using the bilt in function is_valid, storing the user entred data into the Review model using form.cleaned_data and send a redirect and print the data "form.cleaned_data".
     if request.method == 'POST':
         form = ReviewForm(request.POST)
 
         if form.is_valid():
+            review = Review(
+                user_name=form.cleaned_data['user_name'], review_text=form.cleaned_data['review_text'], rating=form.cleaned_data['rating'])
+            review.save()
             print(form.cleaned_data)
             return HttpResponseRedirect("/thank-you")
     else:
