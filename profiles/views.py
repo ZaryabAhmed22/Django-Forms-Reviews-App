7d25072329455c3ecb95566c2989b9b0bb3bcdf4
ZaryabAhmed22 <79729046+ZaryabAhmed22@users.noreply.github.com>
2023-05-18 19:03:00 +0500

    Configuring form for file uploads

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -9,4 +10,6 @@ class CreateProfileView(View):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        pass
+        # Since request.POST only contains none file data of a form, the data containing any kinf of files can be accesses using request.FILES["name_of_input"]
+        print(request.FILES["image"])
+        return HttpResponseRedirect("/profiles")
