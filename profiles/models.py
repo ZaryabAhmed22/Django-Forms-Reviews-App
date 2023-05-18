0456056c44a963a95337489016bc5a25a0edf6ac
ZaryabAhmed22 <79729046+ZaryabAhmed22@users.noreply.github.com>
2023-05-18 20:25:45 +0500

    Adding the file in model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    # BASIC IDEA >> So, we do not ideally store the image on the DB, we only store the path on the DB and Django saves the image in one of our folders in our project folder. We specify the folder by creating it manually and then declaring anew setting in the settings.py " MEDIA_ROOT = BASE_DIR / "uploads" ". Through upload_to, a new sub-folder in uploads wwill be created with the name of images in which all the images of this input field will be saved
+    image = models.FileField(upload_to="images")
--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from .forms import FileForm
+from .models import UserProfile
 
 # Helper functions
 
@@ -21,12 +22,14 @@ class CreateProfileView(View):
         return render(request, "profiles/create_profile.html", {"form": form})
 
     def post(self, request):
-        submitted_form = FileForm(request.FILES, request.POST)
+        submitted_form = FileForm(request.POST, request.FILES)
 
         if submitted_form.is_valid():
-            store_file(request.FILES["image"])
+            # store_file(request.FILES["image"])
             # Since request.POST only contains none file data of a form, the data containing any kinf of files can be accesses using request.FILES["name_of_input"]
-            print(request.FILES["image"])
+            print(request.FILES["user_image"])
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
             return HttpResponseRedirect("/profiles")
 
         return render(request, "profiles/create_profile.html", {"form": submitted_form})
